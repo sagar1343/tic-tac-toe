c35c6c33c4d05c90eeb5cd43c2ab435e0f743ba3
Sagar <sagarchakrawarti24@gmail.com>
2024-02-08 03:33:58 +0530

    completed logic of winning for diagonal and oppositeDiagonal

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const winPage = document.querySelector(".winPage");
 const restart = document.getElementById("restart");
 const winnerName = document.querySelector("#winnerName");
 const boxes = document.querySelectorAll(".box");
+
+restart.addEventListener("click", () => location.reload());
+
 boxes.forEach((box) =>
   box.addEventListener("click", (event) => marking(event.target), {
     once: true,
@@ -21,54 +24,83 @@ let matrix = {
   8: [2, 1],
   9: [2, 2],
 };
-
 function marking(box) {
   if (cross) {
     box.innerText = "X";
     X.push(box.id);
-    if (horizontalWin(X) || verticalWin(X)) {
-      winPage.style.bottom = 0;
-      winnerName.innerText = "Player 1";
-    }
+    checkWinner(X, 1);
   } else {
     box.innerText = "O";
     O.push(box.id);
-    if (horizontalWin(O) || verticalWin(O)) {
-      winPage.style.bottom = 0;
-      winnerName.innerText = "Player 1";
-    }
+    checkWinner(O, 2);
   }
-  console.log(X, O);
   cross = !cross;
 }
 
-function horizontalWin(playerArray) {
-  let win = false;
-  for (let j = 0; j < 3; j++) {
-    let count = 0;
-    for (let i = 0; i < playerArray.length; i++) {
-      if (matrix[playerArray[i]][0] == j) {
-        count++;
+function checkWinner(player, number) {
+  if (
+    horizontalWin(player) ||
+    verticalWin(player) ||
+    diagonalWin(player) ||
+    oppDiagonalWin(player)
+  ) {
+    winPage.style.bottom = 0;
+    winnerName.innerHTML = `Player ${number} is Winner`;
+  }
+  function horizontalWin(playerArray) {
+    let win = false;
+    for (let j = 0; j < 3; j++) {
+      let count = 0;
+      for (let i = 0; i < playerArray.length; i++) {
+        if (matrix[playerArray[i]][0] == j) {
+          count++;
+        }
+      }
+      if (count == 3) win = true;
+    }
+    return win;
+  }
+  function verticalWin(playerArray) {
+    let win = false;
+    for (let j = 0; j < 3; j++) {
+      let count = 0;
+      for (let i = 0; i < playerArray.length; i++) {
+        if (matrix[playerArray[i]][1] == j) {
+          count++;
+        }
+      }
+      if (count == 3) {
+        win = true;
       }
     }
-    if (count == 3) {
-      win = true;
-    }
+    return win;
   }
-  return win;
-}
-function verticalWin(playerArray) {
-  let win = false;
-  for (let j = 0; j < 3; j++) {
-    let count = 0;
-    for (let i = 0; i < playerArray.length; i++) {
-      if (matrix[playerArray[i]][1] == j) {
-        count++;
+  function diagonalWin(playerArray) {
+    let win = false;
+    for (let j = 0; j < 3; j++) {
+      let count = 0;
+      for (let i = 0; i < playerArray.length; i++) {
+        //(0,0),(1,1),(2,2)
+        if (matrix[playerArray[i]][0] == matrix[playerArray[i]][1]) {
+          count++;
+        }
       }
+      if (count == 3) win = true;
     }
-    if (count == 3) {
-      win = true;
-    }
+    return win;
   }
-  return win;
+  function oppDiagonalWin(playerArray) {
+    let win = false;
+    for (let j = 0; j < 3; j++) {
+      let count = 0;
+      for (let i = 0; i < playerArray.length; i++) {
+        // (0,2),(1,1),(2,0)
+        if (matrix[playerArray[i]][0] + matrix[playerArray[i]][1] == 2) {
+          count++;
+        }
+      }
+      if (count == 3) win = true;
+    }
+    return win;
+  }
 }
